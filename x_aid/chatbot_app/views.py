2ea2616fdeb0299d7b9d43596413d2041fe3697d
Sai <pragnaan98@gmail.com>
2025-03-29 03:00:00 +0530

    Make a Connection with backend

--- x_aid/chatbot_app/views.py
+++ x_aid/chatbot_app/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from .models import Chat
+import json
 import os
 
 # Create your views here.
 def chatbot(request):
     chats=Chat.objects.filter(user=request.user)
-    # if request.method=="POST":
-
+    if request.method=="POST":
+        data=json.loads(request.body)
+        message=data.get("message")
+        # return JsonResponse({"message":message,"response": response})
+        return JsonResponse({"reply": f"You said: {message}"})
     return render(request,"chatbot_app/chatbot.html",{'chats':chats})
 
 def disease_prediction(request):
---
+++ x_aid/static/js/chatbot.js
@@ -0,0 +1,44 @@
+console.log("hi");
+
+const messageList = document.querySelector(".chat-container");
+const messageForm = document.querySelector(".message-form");
+const messageInput = document.querySelector(".input-query");
+
+messageForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const message = messageInput.value.trim();
+    // console.log(message)
+    if (message.length == 0) {
+        return;
+    }
+    const messageItem = document.createElement("div");
+    messageItem.classList.add("chat-message", "user-message");
+    messageItem.innerHTML = `
+        <p>${message}</p>
+    `;
+    messageList.appendChild(messageItem);
+    messageInput.value = "";
+
+    const getFacts = async () => {
+        let reponse = await fetch("/chatbot/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": document.querySelector(
+                    "[name=csrfmiddlewaretoken]"
+                ).value,
+            },
+            body: JSON.stringify({ message: message }),
+        });
+        console.log(reponse)
+        data = await reponse.json();
+        const reply = data.reply;
+        const messageItem = document.createElement("div");
+        messageItem.classList.add("chat-message", "bot-message");
+        messageItem.innerHTML = `
+            <p>${reply}</p>
+        `;
+        messageList.appendChild(messageItem);
+    };
+    getFacts(); 
+});
